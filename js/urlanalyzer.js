3f1f5dab62f90375b42197870ca7469a59450b90
Evan Almonte <ealmonte102@qc.cuny.edu>
2018-09-24 01:26:21 -0400

    Add charts to the hsummary portion of the page

--- js/urlanalyzer.js
+++ js/urlanalyzer.js
@@ -13,17 +13,21 @@ function analyzeFile() {
   reader.onload = function(event) {
     var dnsResolverURL = "https://dns.google.com/resolve?name=";
     var fileContent = event.target.result.split('\n');
-    var protocolUsage = {https: 0, http: 0, other: 0};
+    var protocolUsage = {
+      https: 0,
+      http: 0,
+      other: 0
+    };
     var portUsage = {};
     for (var i = 0; i < fileContent.length; ++i) {
       try {
-        if(!(fileContent[i].startsWith("http://") || fileContent[i].startsWith("https://"))) {
+        if (!(fileContent[i].startsWith("http://") || fileContent[i].startsWith("https://"))) {
           fileContent[i] = "http://" + fileContent[i];
         }
         const currentURL = new URL(fileContent[i]);
         var urlToResolve = dnsResolverURL + currentURL.host;
         var row = document.createElement("tr");
-        var schemeColumn = document.createElement("td");
+        var protocolColumn = document.createElement("td");
         var userInfoColumn = document.createElement("td");
         var hostColumn = document.createElement("td");
         var portColumn = document.createElement("td");
@@ -31,15 +35,15 @@ function analyzeFile() {
         var queryColumn = document.createElement("td");
         var fragmentColumn = document.createElement("td");
         var ipColumn = document.createElement("td");
-        var scheme = currentURL.protocol.substring(0, currentURL.protocol.length - 1);
-        schemeColumn.innerHTML = scheme
+        var protocol = currentURL.protocol.substring(0, currentURL.protocol.length - 1);
+        protocolColumn.innerHTML = protocol
         userInfoColumn.innerHTML = currentURL.username;
         hostColumn.innerHTML = currentURL.hostname;
         portColumn.innerHTML = currentURL.port;
         pathColumn.innerHTML = currentURL.pathname;
         queryColumn.innerHTML = currentURL.search;
         fragmentColumn.innerHTML = currentURL.hash;
-        row.appendChild(schemeColumn);
+        row.appendChild(protocolColumn);
         row.appendChild(userInfoColumn);
         row.appendChild(hostColumn);
         row.appendChild(portColumn);
@@ -48,24 +52,60 @@ function analyzeFile() {
         row.appendChild(fragmentColumn);
         row.appendChild(ipColumn);
         tableBody.appendChild(row);
-        protocolUsage[scheme]++;
-        var port = currentURL.port == "" ? "none" : "" + currentURL.port;
-        if(port in portUsage) {
-          portUsage[port]++;
+        protocolUsage[protocol]++;
+        var portUsed = getPort(protocol, currentURL.port);
+        if(portUsed in portUsage) {
+          portUsage[portUsed]++;
         } else {
-          portUsage[port] = 0;
+          portUsage[portUsed] = 1;
         }
         makeDNSRequest(urlToResolve, ipColumn);
       } catch (err) {
         console.log(err);
       }
     }
+    var protocolData = [{
+        x: "HTTPS",
+        value: protocolUsage["https"]
+      },
+      {
+        x: "HTTP",
+        value: protocolUsage["http"]
+      },
+      {
+        x: "NONE",
+        value: protocolUsage
+      }
+    ]
+    var portData = [];
+    var keys = Object.keys(portUsage)
+    for(var index in keys) {
+      portData.push({
+        x: keys[index],
+        value: portUsage[keys[index]]
+      });
+    }
+    console.log(portData);
+    createChart(protocolData, "Protocol Usage", "protocol-chart-container");
+    createChart(portData, "Port Usage", "port-chart-container");
   };
 
   reader.onerror = function(event) {
     console.error("File could not be read! Code " + event.target.error.code);
   };
   reader.readAsText(fileChosen);
+}
+
+function getPort(protocolUsed, portUsed) {
+  if(portUsed === "") {
+    switch(protocolUsed) {
+      case "https":
+        return "443";
+      case "http":
+        return "80";
+    }
+  }
+  return portUsed;
 }
 
 function makeDNSRequest(urlToResolve, ipColumn) {
@@ -78,4 +118,12 @@ function makeDNSRequest(urlToResolve, ipColumn) {
   };
   xhttp.open("GET", urlToResolve, true);
   xhttp.send();
+}
+
+function createChart(data, title, container) {
+  var chart = anychart.pie();
+  chart.title(title);
+  chart.data(data);
+  chart.container(container);
+  chart.draw();
 }
